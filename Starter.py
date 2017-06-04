54cb9931faf642b8521c744b087b14e048b0f657
Nirosha201 <nirosha201@gmail.com>
2017-06-04 12:04:50 +1200

    Main interface with options to choose

--- Starter.py
+++ Starter.py
@@ -10,11 +10,13 @@ import interface_pyqtgraph
 import interface_FindSpecies
 import Dialogs
 
+DOC=True    # only DOC features or all
+
 # Start the application
 app = QApplication(sys.argv)
 
 # This screen asks what you want to do, then gets the response
-first = Dialogs.StartScreen()
+first = Dialogs.StartScreen(DOC=DOC)
 first.setWindowIcon(QIcon('img/AviaNZ.ico'))
 first.show()
 app.exec_()
@@ -23,14 +25,16 @@ task = first.getValues()
 # print task
 
 if task == 1:
-    avianz = interface_pyqtgraph.AviaNZInterface(configfile='AviaNZconfig.txt')
+    avianz = interface_pyqtgraph.AviaNZInterface(configfile='AviaNZconfig.txt',DOC=DOC)
     avianz.setWindowIcon(QIcon('img/AviaNZ.ico'))
     avianz.show()
     app.exec_()
-elif task==2:
+if task==2:
     avianz = interface_FindSpecies.AviaNZFindSpeciesInterface(configfile='AviaNZconfig.txt')
     avianz.setWindowIcon(QIcon('img/AviaNZ.ico'))
     avianz.show()
     app.exec_()
-else:
+elif task==3:
     app.exit()
+
+
