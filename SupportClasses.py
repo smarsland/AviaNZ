dbdcd969dd24aa53a8d2907d870b5f0095c65033
Nirosha <nirosha201@gmail.com>
2017-07-19 14:45:41 +1200

    minor changes

--- SupportClasses.py
+++ SupportClasses.py
@@ -9,7 +9,7 @@ from PyQt4.QtGui import *
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph.functions as fn
-
+import pyqtgraph.exporters as pge
 
 class TimeAxis(pg.AxisItem):
     # Time axis (at bottom of spectrogram)
@@ -238,6 +238,64 @@ class FlowLayout(QtGui.QLayout):
             lineHeight = max(lineHeight, item.sizeHint().height())
 
         return y + lineHeight - rect.y()
+
+# class FixedImageExporter(pge.ImageExporter):
+#     def __init__(self, *args, **kwargs):
+#         super(pge.ImageExporter, self).__init__(*args, **kwargs)
+#
+#     def export(self, fileName=None, toBytes=False, copy=False):
+#         if fileName is None and not toBytes and not copy:
+#             if USE_PYSIDE:
+#                 filter = ["*."+str(f) for f in QtGui.QImageWriter.supportedImageFormats()]
+#             else:
+#                 filter = ["*."+bytes(f).decode('utf-8') for f in QtGui.QImageWriter.supportedImageFormats()]
+#             preferred = ['*.png', '*.tif', '*.jpg']
+#             for p in preferred[::-1]:
+#                 if p in filter:
+#                     filter.remove(p)
+#                     filter.insert(0, p)
+#             self.fileSaveDialog(filter=filter)
+#             return
+#
+#         targetRect = QtCore.QRect(0, 0, self.params['width'], self.params['height'])
+#         sourceRect = self.getSourceRect()
+#
+#
+#         #self.png = QtGui.QImage(targetRect.size(), QtGui.QImage.Format_ARGB32)
+#         #self.png.fill(pyqtgraph.mkColor(self.params['background']))
+#         w, h = self.params['width'], self.params['height']
+#         if w == 0 or h == 0:
+#             raise Exception("Cannot export image with size=0 (requested export size is %dx%d)" % (w,h))
+#         bg = np.empty((int(self.params['width']), int(self.params['height']), 4), dtype=np.ubyte)
+#         color = self.params['background']
+#         bg[:,:,0] = color.blue()
+#         bg[:,:,1] = color.green()
+#         bg[:,:,2] = color.red()
+#         bg[:,:,3] = color.alpha()
+#         self.png = pg.makeQImage(bg, alpha=True)
+#
+#         ## set resolution of image:
+#         origTargetRect = self.getTargetRect()
+#         resolutionScale = targetRect.width() / origTargetRect.width()
+#         #self.png.setDotsPerMeterX(self.png.dotsPerMeterX() * resolutionScale)
+#         #self.png.setDotsPerMeterY(self.png.dotsPerMeterY() * resolutionScale)
+#
+#         painter = QtGui.QPainter(self.png)
+#         #dtr = painter.deviceTransform()
+#         try:
+#             self.setExportMode(True, {'antialias': self.params['antialias'], 'background': self.params['background'], 'painter': painter, 'resolutionScale': resolutionScale})
+#             painter.setRenderHint(QtGui.QPainter.Antialiasing, self.params['antialias'])
+#             self.getScene().render(painter, QtCore.QRectF(targetRect), QtCore.QRectF(sourceRect))
+#         finally:
+#             self.setExportMode(False)
+#         painter.end()
+#
+#         if copy:
+#             QtGui.QApplication.clipboard().setImage(self.png)
+#         elif toBytes:
+#             return self.png
+#         else:
+#             self.png.save(fileName)
 
 # Helper functions
 def splitFile5mins(self, name):
