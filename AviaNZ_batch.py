5711d4a5d90b4af6093d465a64d1bace109e6ee2
Stephen Marsland <stephen.marsland@vuw.ac.nz>
2018-10-23 14:41:25 +1300

    Zooniverse creation code

--- AviaNZ_batch.py
+++ AviaNZ_batch.py
@@ -1,3 +1,25 @@
+
+# AviaNZ_batch.py
+#
+# This is the proceesing class for the batch AviaNZ interface
+# Version 1.3 23/10/18
+# Authors: Stephen Marsland, Nirosha Priyadarshani, Julius Juodakis
+
+#    AviaNZ birdsong analysis program
+#    Copyright (C) 2017--2018
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os, re, platform, fnmatch
 
 from PyQt5.QtGui import *
--- colourMaps.py
+++ colourMaps.py
@@ -1,8 +1,27 @@
-# Version 0.2 10/7/17
-# Author: Stephen Marsland
 
+# colourMaps.py
+
+# Version 1.3 23/10/18
 # Set of colour maps for the AviaNZ interface.
 # These were made using cmaps.py in test
+# Authors: Stephen Marsland, Nirosha Priyadarshani, Julius Juodakis
+
+#    AviaNZ birdsong analysis program
+#    Copyright (C) 2017--2018
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import numpy as np
 
