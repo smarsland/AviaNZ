202a85b226b1ee13a7dc125515413f2bb6eeb4e6
Stephen Marsland <stephen.marsland@vuw.ac.nz>
2018-10-23 15:00:34 +1300

    Bigger font for zooniverse text

--- AviaNZ.py
+++ AviaNZ.py
@@ -81,7 +81,7 @@ class AviaNZ(QMainWindow):
         super(AviaNZ, self).__init__()
         self.root = root
         self.CLI = CLI
-        self.cheatsheet = cheatsheet
+        self.cheatsheet = True #cheatsheet
 
         # At this point, the main config file should already be ensured to exist.
         self.configdir = configdir
@@ -1617,25 +1617,29 @@ class AviaNZ(QMainWindow):
         SpecRange = FreqRange/height
         
         if self.cheatsheet:
-            # Complete me!
-            offset=4
-            self.label1 = pg.TextItem(text=str(0), color='g', anchor=(0,0))
+            offset=6
+            txt='<span style="color: #0F0; font-size:20pt">%s</div>'%str(0)
+            self.label1 = pg.TextItem(html=txt, color='g', anchor=(0,0))
             self.p_spec.addItem(self.label1)
             self.label1.setPos(0,0+offset)
 
-            self.label2 = pg.TextItem(text=str(FreqRange//4000), color='g', anchor=(0,0))
+            txt='<span style="color: #0F0; font-size:20pt">%s</div>'%str(FreqRange//4000)
+            self.label2 = pg.TextItem(html=txt, color='g', anchor=(0,0))
             self.p_spec.addItem(self.label2)
             self.label2.setPos(0,SpecRange/4+offset)
 
-            self.label3 = pg.TextItem(text=str(FreqRange//2000), color='g', anchor=(0,0))
+            txt='<span style="color: #0F0; font-size:20pt">%s</div>'%str(FreqRange//2000)
+            self.label3 = pg.TextItem(html=txt, color='g', anchor=(0,0))
             self.p_spec.addItem(self.label3)
             self.label3.setPos(0,SpecRange/2+offset)
 
-            self.label4 = pg.TextItem(text=str(3*FreqRange//4000), color='g', anchor=(0,0))
+            txt='<span style="color: #0F0; font-size:20pt">%s</div>'%str(3*FreqRange//4000)
+            self.label4 = pg.TextItem(html=txt, color='g', anchor=(0,0))
             self.p_spec.addItem(self.label4)
             self.label4.setPos(0,3*SpecRange/4+offset)
 
-            self.label5 = pg.TextItem(text=str(FreqRange//1000), color='g', anchor=(0,0))
+            txt='<span style="color: #0F0; font-size:20pt">%s</div>'%str(FreqRange//1000)
+            self.label5 = pg.TextItem(html=txt, color='g', anchor=(0,0))
             self.p_spec.addItem(self.label5)
             self.label5.setPos(0,SpecRange+offset)
         else:
