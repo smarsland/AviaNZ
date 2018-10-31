4da0a82544fb9499af906d77487a2e9852fc2576
Stephen <stephen.marsland@vuw.ac.nz>
2018-11-01 12:04:05 +1300

    Separate zooniverse flag

--- make_zooniverse.sh
+++ make_zooniverse.sh
@@ -1,8 +1,11 @@
 
-python make_zooniverse.py -s 'Kiwi, Nth Is Brown' -i 'Sound Files/Batch/' -o TestOut/KNIB
-python AviaNZ.py -c -s -f TestOut -o TestOut
+python make_zooniverse.py -s 'Kiwi (Nth Is Brown)' -i 'Sound Files/Batch2/' -o TestOut/KNIB
+python AviaNZ.py -c -z -f TestOut -o TestOut
 
 # On laptop, have to do conda remove ffmpeg first
 for f in ls TestOut/*.wav; do fn="${f%%.*}"; ffmpeg -i $fn.wav $fn.mp3; done
 for f in ls TestOut/*.png; do convert $f -trim $f ; done
+
+# This line copies all the data files in their folders and puts them elsewhere
+# rsync -avm --include='*.data' -f 'hide,! */' . TestOut
 
