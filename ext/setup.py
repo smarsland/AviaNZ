dd312c8cec7a42c7630ce77ce03f6631060df53d
Stephen <stephen.marsland@vuw.ac.nz>
2018-11-15 11:21:05 +1300

    Ready for WVD compilation

--- ext/setup.py
+++ ext/setup.py
@@ -6,9 +6,15 @@ setup(
 	cmdclass={'build_ext': build_ext},
 	name = 'ce_denoise',
 	description = 'C extensions for denoising',
-	ext_modules=[Extension("ce_denoise",
+	ext_modules=[
+        Extension("ce_denoise",
 		sources=["ce_denoise.pyx", "ce_functions.c"],
-		include_dirs=[numpy.get_include()])]
+		include_dirs=[numpy.get_include()])
+        #Extension("ce_WignerVille",
+		#sources=["ce_wvd.pyx", "ce_wvd_functions.c"],
+		#include_dirs=[numpy.get_include()])
+
+]
 )
 
 # installation: python setup.py build_ext -i
