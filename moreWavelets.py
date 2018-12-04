3e93cd034c2592a72f5830e92b8d1a9337c95aec
jjuod <j.juodakis@gmail.com>
2018-12-05 10:53:47 +1300

    some fiddling

--- moreWavelets.py
+++ moreWavelets.py
@@ -60,80 +60,98 @@ t = np.linspace(0,5+47/400,2048)
 
 sines = np.sin(30*np.pi*t) + np.sin(60*np.pi*t) + np.sin(90*np.pi*t) + np.sin(120*np.pi*t) + np.sin(180*np.pi*t)
 rest = 2*np.exp(-30*t) * np.sin(260*np.pi*t)
-inds = np.where(t<0.125)
+# inds = np.where(t<0.125)
+# sines[inds] += rest[inds]
+# inds = np.where((t>0.3725) & (t<0.5))
+# sines[inds] += rest[inds]
+# inds = np.where((t>0.7475) & (t<5.1175))
+# sines[inds] += rest[inds]
+inds = np.where(t>0.5) and np.where(t<0.7475)
 sines[inds] += rest[inds]
-inds = np.where((t>0.3725) & (t<0.5))
+inds = np.where(t>0.125) and np.where(t<0.3725)
 sines[inds] += rest[inds]
-inds = np.where((t>0.7475) & (t<5.1175))
-sines[inds] += rest[inds]
-
+ 
 pl.ion()
-pl.figure()
-pl.plot(t[:300],sines[:300])
+#pl.figure()
+#pl.plot(t[:300],sines[:300])
 
 wp = pywt.WaveletPacket(data=sines, wavelet='db4',maxlevel=3)
-l = 400
+l = len(wp.data)
 
 fig,axes = pl.subplots(6,3)
 new_wp = pywt.WaveletPacket(data=None, wavelet='db4',maxlevel=3)
 new_wp['a'] = wp['a'].data
 x = new_wp.reconstruct(update=False)
-axes[0,0].plot(x[:300])
+axes[0,0].plot(x[:l//8])
 ft = fft.fft(x,l)
 ft[l//4:3*l//4] = 0
-axes[0,1].plot(np.abs(ft)[:200])
+axes[0,1].plot(np.abs(ft)[:l//2])
 new = np.real(fft.ifft(ft))
-axes[0,2].plot(new[:300])
+axes[0,2].plot(new[:l//8])
 
 new_wp = pywt.WaveletPacket(data=None, wavelet='db4',maxlevel=3)
 new_wp['d'] = wp['d'].data
 x = new_wp.reconstruct(update=False)
-axes[1,0].plot(x[:300])
+axes[1,0].plot(x[:l//8])
 ft = fft.fft(x,l)
 ft[:l//4] = 0
 ft[3*l//4:] = 0
-axes[1,1].plot(np.abs(ft)[:200])
-new = np.real(fft.ifft(ft))
-axes[1,2].plot(new[:300])
+axes[1,1].plot(np.abs(ft)[:l//2])
+new2 = np.real(fft.ifft(ft))
+axes[1,2].plot(new2[:l//8])
+
+sines2 = new[0::2]
+wp = pywt.WaveletPacket(data=sines2, wavelet='db4',maxlevel=3)
+l = l //2
 
 new_wp = pywt.WaveletPacket(data=None, wavelet='db4',maxlevel=3)
-new_wp['aa'] = wp['aa'].data
+new_wp['a'] = wp['a'].data
 x = new_wp.reconstruct(update=False)
-axes[2,0].plot(x[:300])
+axes[2,0].plot(x[:l//8])
 ft = fft.fft(x,l)
-ft[l//8:7*l//8] = 0
-axes[2,1].plot(np.abs(ft)[:200])
+ft[l//4:3*l//4] = 0
+#ft[l//8:7*l//8] = 0
+axes[2,1].plot(np.abs(ft)[:l//2*2])
 new = np.real(fft.ifft(ft))
-axes[2,2].plot(new[:300])
+axes[2,2].plot(new[:l//8])
 
 new_wp = pywt.WaveletPacket(data=None, wavelet='db4',maxlevel=3)
-new_wp['ad'] = wp['ad'].data
+new_wp['d'] = wp['d'].data
 x = new_wp.reconstruct(update=False)
-axes[3,0].plot(x[:300])
+axes[3,0].plot(x[:l//8])
 ft = fft.fft(x,l)
-ft[:l//8] = 0
-ft[3*l//8:] = 0
-axes[3,1].plot(np.abs(ft)[:200])
-new = np.real(fft.ifft(ft))
-axes[3,2].plot(new[:300])
+ft[:l//4] = 0
+ft[3*l//4:] = 0
+#ft[:l//8] = 0
+#ft[2*l//8:] = 0
+axes[3,1].plot(np.abs(ft)[:l//2*2])
+new3 = np.real(fft.ifft(ft))
+axes[3,2].plot(new[:l//8])
+
+sines3 = new3[0::2]
+wp = pywt.WaveletPacket(data=sines3, wavelet='db4',maxlevel=3)
+l = l //2
 
 new_wp = pywt.WaveletPacket(data=None, wavelet='db4',maxlevel=3)
-new_wp['aaa'] = wp['aaa'].data
+new_wp['a'] = wp['a'].data
 x = new_wp.reconstruct(update=False)
-axes[4,0].plot(x[:300])
+axes[4,0].plot(x[:l//8])
 ft = fft.fft(x,l)
-ft[l//16:15*l//16] = 0
-axes[4,1].plot(np.abs(ft)[:200])
+ft[l//4:3*l//4] = 0
+#ft[l//16:15*l//16] = 0
+axes[4,1].plot(np.abs(ft)[:l//2*4])
 new = np.real(fft.ifft(ft))
-axes[4,2].plot(new[:300])
+axes[4,2].plot(new[:l//8])
 
 new_wp = pywt.WaveletPacket(data=None, wavelet='db4',maxlevel=3)
-new_wp['aad'] = wp['aad'].data
+new_wp['d'] = wp['d'].data
 x = new_wp.reconstruct(update=False)
-axes[5,0].plot(x[:300])
+axes[5,0].plot(x[:l//8])
 ft = fft.fft(x,l)
-ft[:l//16] = 0
-ft[3*l//16:] = 0
-axes[5,1].plot(np.abs(ft)[:200])
+ft[:l//4] = 0
+ft[3*l//4:] = 0
+#ft[:l//16] = 0
+#ft[2*l//16:] = 0
+axes[5,1].plot(np.abs(ft)[:l//2*4])
 new = np.real(fft.ifft(ft))
-axes[5,2].plot(new[:300])
+axes[5,2].plot(new[:l//8])
