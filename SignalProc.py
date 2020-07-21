0946b1cd086a0edcfa33e5003095a38abe90c297
jjuod <j.juodakis@gmail.com>
2020-07-22 08:55:20 +1200

    some old bug fixes

--- SignalProc.py
+++ SignalProc.py
@@ -341,9 +341,9 @@ class SignalProc:
         lowStop = lowPass-band
         highStop = highPass+band
         # safety checks for values near edges
-        if lowStop<0:
+        if lowStop<=0:
             lowStop = lowPass/2
-        if highStop>1:
+        if highStop>=1:
             highStop = (1+highPass)/2
 
         if lowPass == 0 and highPass == 1:
