d424b9617b945e50d56d83a35eeb9b3938d4c4d8
Virginia Listanti <vlistanti.88@gmail.com>
2020-08-14 15:09:50 +1200

    cleaned code for invert spectrogram. see convert_image_2sound.py

--- convert_image_2sound.py
+++ convert_image_2sound.py
@@ -30,6 +30,15 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import wavio
 
+"""
+This script works in batch. It generate sound files from .bmp spectrogram images.
+There are two option (not used is commented)
+1) Audible file -> freqnecy shift to hear bat echolocation [ON]
+2) Same frequency band -> just the spectrogram inverted [OFF]
+
+NOTE: we need appropriate number of frequency bins in order to make invertSpectrogram work
+"""
+
 #dirName='/home/listanvirg/Data/Bat/BAT/TEST_DATA/'
 #dirName='D:\\Desktop\\Documents\\Work\\Data\\Bat\\BAT\\TRAIN_DATA\\NONE'
 dirName='D:\\Desktop\\Documents\\Work\\Data\\Bat\\BAT\\CNN experiment\\TRAIN4'
@@ -44,13 +53,15 @@ for root, dirs, files in os.walk(str(dirName)):
             img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert to grayscale
             img2[-1, :] = 254 * np.ones((np.shape(img2)[1])) #cut last row
 
-            # to have an audible file
-            #row_dim = 7 * np.shape(img2)[0]
-            #appo = 254 * np.ones((row_dim, np.shape(img2)[1]))
-            #spec = np.concatenate((appo, img2))
+            # OPTION 1: AUFIBLE FILE
+            row_dim = 7 * np.shape(img2)[0]
+            appo = 254 * np.ones((row_dim, np.shape(img2)[1]))
+            spec = np.concatenate((appo, img2))
+            samplerate = 16000
 
-            # same frequyency band
-            spec=np.repeat(img2,8, axis=0) #repeat rows 7 times to fit invertspectrogram
+            #OPTION 2: same frequency band
+            #spec=np.repeat(img2,8, axis=0) #repeat rows 7 times to fit invertspectrogram
+            # samplerate = 176000
 
             spec = -(spec - 254.0 * np.ones(np.shape(spec)))  # reverse value having the black as the most intense
             spec=np.flipud(spec) #reverse up and down of the spectrogram -> see AviaNZ spectrogram
@@ -60,11 +71,10 @@ for root, dirs, files in os.walk(str(dirName)):
             (n, m) = np.shape(spec)
             a = sp.SignalProc(window_width=1024, incr=512)
             wave = a.invertSpectrogram(spec2, 1024, 512)
-            #samplerate = 176000
-            samplerate = 16000
-            #wave = a.bandpassFilter(wave, samplerate, 1000, 22000)
+
+            #wave = a.bandpassFilter(wave, samplerate, 1000, 22000) bandpass?
             wavFile = bmpFile + '.wav'
-            #wavFile=bmpFile+'_audible.wav' #to make sound audible
+            #wavFile=bmpFile+'_audible.wav' #Other option
             wavio.write(wavFile, wave, samplerate, sampwidth=2)
 
 
