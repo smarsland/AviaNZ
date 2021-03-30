485c36b70d4277e1548fd448ee925d75e0f0329d
VirgyLis <vlistanti.88@gmail.com>
2021-03-30 17:54:30 +1300

    Update IF.py

--- IF.py
+++ IF.py
@@ -155,8 +155,164 @@ class IF:
         #convert to absolute values, since we need only them; also improves computational speed as TFR is no more complex and is positive
 
         nfunc=np.ones((NF,1))
-        tn1=find(~isnan(TFR(end,:)),1,'first'); tn2=find(~isnan(TFR(end,:)),1,'last'); sflag=0;
+        tn1=np.argwhere(np.isnan(TFR[-1,:]))[0] #find first index where TFR is NAN of last line
+        tn2=np.argwhere(np.isnan(TFR[-1,:]))[-1] #find last index where TFR is NAN of last line
+        sflag=0;
+        
+
+        if (type(method) is char and method.lower!='max') or length(method)==1: #if not frequency-based or maximum-based extraction
+            sflag=1
+            #----------------------------------------------------------------------
+            #Construct matrices of ridge indices, frequencies and amplitudes:
+            #[Ip],[Fp],[Qp], respectively; [Np] - number of peaks at each time.
+            if ~isempty(Skel):
+                Np=Skel{1}; Ip=Skel{2}; Fp=Skel{3}; Qp=Skel{4}; Mp=max(Np);
+            else
+                if ~strcmpi(DispMode,'off') && ~strcmpi(DispMode,'notify')
+                    fprintf('Locating the amplitude peaks in TFR... ');
+                end
+                TFR=vertcat(zeros(1,L),TFR,zeros(1,L)); %pad TFR with zeros
+                idft=1+find(TFR(2:end-1)>=TFR(1:end-2) & TFR(2:end-1)>TFR(3:end)); %find linear indices of the peaks
+                [idf,idt]=ind2sub(size(TFR),idft); idf=idf-1; %find frequency and time indices of the peaks
+                idb=find(idf==1 | idf==NF); idft(idb)=[]; idf(idb)=[]; idt(idb)=[]; %remove the border peaks
+                dind=[0;find(diff(idt(:))>0);length(idt)]; Mp=max([max(diff(dind)),2]);
+                Np=zeros(1,L); idn=zeros(length(idt),1);
+                for dn=1:length(dind)-1,
+                    ii=dind(dn)+1:dind(dn+1); idn(ii)=1:length(ii);
+                    Np(idt(ii(1)))=length(ii);
+                end
+                idnt=sub2ind([Mp,L],idn(:),idt(:));
+                %Quadratic interpolation to better locate the peaks
+                a1=TFR(idft-1); a2=TFR(idft); a3=TFR(idft+1);
+                dp=(1/2)*(a1-a3)./(a1-2*a2+a3);
+                %Assign all
+                Ip=ones(Mp,L)*NaN; Fp=ones(Mp,L)*NaN; Qp=ones(Mp,L)*NaN;
+                Ip(idnt)=idf+dp; Qp(idnt)=a2-(1/4)*(a1-a3).*dp;
+                if fres==1, Fp(idnt)=freq(idf)+dp(:)*fstep;
+                else Fp(idnt)=freq(idf).*exp(dp(:)*fstep); end
+                %Correct "bad" places, if present
+                idb=find(isnan(dp) | abs(dp)>1 | idf==1 | idf==NF);
+                if ~isempty(idb)
+                    Ip(idnt(idb))=idf(idb);
+                    Fp(idnt(idb))=freq(idf(idb));
+                    Qp(idnt(idb))=a2(idb);
+                end
+                %Remove zeros and clear the indices
+                TFR=TFR(2:end-1,:); clear idft idf idt idn dind idnt a1 a2 a3 dp;
+                %Display
+                if ~strcmpi(DispMode,'off')
+                    if ~strcmpi(DispMode,'notify')
+                        fprintf('(number of ridges: %d+-%d, from %d to %d)\n',round(mean(Np(tn1:tn2))),round(std(Np(tn1:tn2))),min(Np),max(Np));
+                    end
+                    idb=find(Np(tn1:tn2)==0); NB=length(idb);
+                    if NB>0, fprintf(2,sprintf('Warning: At %d times there are no peaks (using border points instead).\n',NB)); end
+                end
+                %If there are no peaks, assign border points
+                idb=find(Np(tn1:tn2)==0); idb=tn1-1+idb; NB=length(idb);
+                if NB>0, G4=abs(TFR([1;2;NF-1;NF],idb)); end
+                for bn=1:NB
+                    tn=idb(bn); cn=1; cg=G4(:,bn);
+                    if cg(1)>cg(2) || cg(4)>cg(3)
+                        if cg(1)>cg(2), Ip(cn,tn)=1; Qp(cn,tn)=cg(1); Fp(cn,tn)=freq(1); cn=cn+1; end
+                        if cg(4)>cg(3), Ip(cn,tn)=NF; Qp(cn,tn)=cg(4); Fp(cn,tn)=freq(NF); cn=cn+1; end
+                    else
+                        Ip(1:2,tn)=[1;NF]; Qp(1:2,tn)=[cg(1);cg(4)]; Fp(1:2,tn)=[freq(1);freq(NF)]; cn=cn+2;
+                    
+                    Np(tn)=cn-1;
+                
+                clear idb NB G4;
+            if nargout>2, varargout{2}={Np,Ip,Fp,Qp}; end
+            if strcmpi(NormMode,'on'), nfunc=tfrnormalize(abs(TFR(:,tn1:tn2)),freq); end
+            ci=Ip; ci(isnan(ci))=NF+2; cm=ci-floor(ci); ci=floor(ci); nfunc=[nfunc(1);nfunc(:);nfunc(end);NaN;NaN];
+            Rp=(1-cm).*nfunc(ci+1)+cm.*nfunc(ci+2); Wp=AmpFunc(Qp.*Rp); nfunc=nfunc(2:end-3); %apply the functional to amplitude peaks
     
+        elseif ~ischar(method) && length(method)>1 %frequency-based extraction
+            if length(method)~=L
+                error('The specified frequency profile ("Method" property) should be of the same length as signal.');
+            
+    
+            efreq=method; submethod=1; if max(abs(imag(efreq)))>0, submethod=2; efreq=imag(efreq); end
+            if ~strcmpi(DispMode,'off') && ~strcmpi(DispMode,'notify')
+                if submethod==1, fprintf('Extracting the ridge curve lying in the same TFR supports as the specified frequency profile.\n');
+                else fprintf('Extracting the ridge curve lying nearest to the specified frequency profile.\n'); end
+            
+    
+            tn1=max([tn1,find(~isnan(efreq),1,'first')]); tn2=min([tn2,find(~isnan(efreq),1,'last')]);
+            if fres==1, eind=1+floor(0.5+(efreq-freq(1))/fstep);
+            else eind=1+floor(0.5+log(efreq/freq(1))/fstep); end
+            eind(eind<1)=1; eind(eind>NF)=NF;
+    
+            %Extract the indices of the peaks
+            for tn=tn1:tn2
+                cind=eind(tn); cs=abs(TFR(:,tn));
+        
+                %Ridge point
+                cpeak=cind;
+                if cind>1 && cind<NF
+                    if cs(cind+1)==cs(cind-1) || submethod==2
+                        cpeak1=cind-1+find(cs(cind:end-1)>=cs(cind-1:end-2) & cs(cind:end-1)>cs(cind+1:end),1,'first'); cpeak1=min([cpeak1,NF]);
+                        cpeak2=cind+1-find(cs(cind:-1:2)>=cs(cind+1:-1:3) & cs(cind:-1:2)>cs(cind-1:-1:1),1,'first'); cpeak2=max([cpeak2,1]);
+                        if cs(cpeak1)>0 && cs(cpeak2)>0
+                            if cpeak1-cind==cind-cpeak2
+                                if cs(cpeak1)>cs(cpeak2), cpeak=cpeak1;
+                                else cpeak=cpeak2; end
+                            elseif cpeak1-cind<cind-cpeak2, cpeak=cpeak1;
+                            elseif cpeak1-cind>cind-cpeak2, cpeak=cpeak2;
+                            end
+                        elseif cs(cpeak1)==0, cpeak=cpeak2;
+                        elseif cs(cpeak2)==0, cpeak=cpeak1;
+                        end
+                    elseif cs(cind+1)>cs(cind-1)
+                        cpeak=cind-1+find(cs(cind:end-1)>=cs(cind-1:end-2) & cs(cind:end-1)>cs(cind+1:end),1,'first'); cpeak=min([cpeak,NF]);
+                    elseif cs(cind+1)<cs(cind-1)
+                        cpeak=cind+1-find(cs(cind:-1:2)>cs(cind-1:-1:1) & cs(cind:-1:2)>=cs(cind+1:-1:3),1,'first'); cpeak=max([cpeak,1]);
+                    end
+                elseif cind==1
+                    if cs(2)<cs(1), cpeak=cind;
+                    else
+                        cpeak=1+find(cs(cind+1:end-1)>=cs(cind:end-2) & cs(cind+1:end-1)>cs(cind+2:end),1,'first'); cpeak=min([cpeak,NF]);
+                    end
+                elseif cind==NF
+                    if cs(NF-1)<cs(NF), cpeak=cind;
+                    else
+                        cpeak=NF-find(cs(cind-1:-1:2)>cs(cind-2:-1:1) & cs(cind-1:-1:2)>=cs(cind:-1:3),1,'first'); cpeak=max([cpeak,1]);
+                    end
+                end
+                tfsupp(1,tn)=cpeak;
+        
+                %Boundaries of time-frequency support
+                iup=[]; idown=[];
+                if cpeak<NF-1, iup=cpeak+find(cs(cpeak+1:end-1)<=cs(cpeak:end-2) & cs(cpeak+1:end-1)<cs(cpeak+2:end),1,'first'); end
+                if cpeak>2, idown=cpeak-find(cs(cpeak-1:-1:2)<=cs(cpeak:-1:3) & cs(cpeak-1:-1:2)<cs(cpeak-2:-1:1),1,'first'); end
+                iup=min([iup,NF]); idown=max([idown,1]);
+                tfsupp(2,tn)=idown; tfsupp(3,tn)=iup;
+            end
+    
+            %Transform to frequencies
+            pind=tfsupp(1,:); tfsupp(:,tn1:tn2)=freq(tfsupp(:,tn1:tn2));
+            pamp(tn1:tn2)=abs(TFR(sub2ind(size(TFR),pind(tn1:tn2),tn1:tn2)));
+    
+            %Optional output arguments
+            if nargout>1
+                ec.efreq=efreq; ec.eind=eind;
+                ec.pfreq=tfsupp(1,:); ec.pind=pind; ec.pamp=pamp; ec.idr=idr;
+                varargout{1}=ec;
+            end
+            if nargout>2, varargout{2}=[]; end
+    
+            %Plotting (if needed)
+            if ~isempty(strfind(DispMode,'plot'))
+                scrsz=get(0,'ScreenSize'); figure('Position',[scrsz(3)/4,scrsz(4)/8,2*scrsz(3)/3,2*scrsz(4)/3]);
+                ax=axes('Position',[0.1,0.1,0.8,0.8],'FontSize',16); hold all;
+                title(ax(1),'Ridge curve \omega_p(t)/2\pi'); ylabel(ax(1),'Frequency (Hz)'); xlabel(ax(1),'Time (s)');
+                plot(ax(1),(0:L-1)/fs,efreq,'--','Color',[0.5,0.5,0.5],'LineWidth',2,'DisplayName','Specified frequency profile');
+                plot(ax(1),(0:L-1)/fs,tfsupp(1,:),'-k','LineWidth',2,'DisplayName','Extracted frequency profile');
+                legend(ax(1),'show'); if fres==2, set(ax(1),'YScale','log'); end
+            end
+            if ~isempty(strfind(PlotMode,'on')), plotfinal(tfsupp,TFR,freq,fs,DispMode,PlotMode); end
+            if nargout>2, varargout{2}=Skel; end
+    
+            return;
     
         return tfsupp,ecinfo,Skel
 
