81ff770c558e3e92c6738b5c3c7e79e213d4b5e2
VirgyLis <vlistanti.88@gmail.com>
2022-03-18 12:01:51 +1300

    Update IF_experiment_cuda3.py

--- IF_experiment_cuda3.py
+++ IF_experiment_cuda3.py
@@ -731,7 +731,7 @@ for spec_type in spectrogram_types:
                 for opt_option in optimization_options:
                     # loop over optimization options
 
-                    if Test_id < 141:
+                    if Test_id < 150:
                         print('Skipping Test ', Test_id)
                         Test_id += 1
                         continue
@@ -757,6 +757,12 @@ for spec_type in spectrogram_types:
                         # CHECK TO SKIP IF OPT_METRIC==IATSENKO and pure_tone signal
                         if opt_metric == 'Iatsenko' and signal_id == 'pure_tone':
                             continue
+
+                        if Test_id == 150:
+                            if signal_id == "linear_downchirp":
+                                continue
+                            if signal_id == "exponential_upchirp":
+                                continue
 
                         folder_path = dataset_dir + '/' + signal_id
                         print("Analysing folder: ", folder_path)
