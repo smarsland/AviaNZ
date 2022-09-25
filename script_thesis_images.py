25eca2eb7b0f907c1d42f3b6d2b12d6e821aea71
VirgyLis <vlistanti.88@gmail.com>
2022-09-25 13:45:18 +1300

    Update script_thesis_images.py

--- script_thesis_images.py
+++ script_thesis_images.py
@@ -38,8 +38,9 @@ freq0 = 1000
 freq1 = 6000
 s1=A*np.sin(phi+instant_phase(t, T, freq0, freq1))
 s0=np.sin(phi+instant_phase(t, T, freq0, freq1))
-np.random.seed(19680801)
-At = np.random.uniform(0.1, 2, len(s0))
+# np.random.seed(19680801)
+# At = np.random.uniform(0.1, 2, len(s0))
+At = 0.0001 +t
 s2 = At * s0
 insta_freq = instant_freq(t, T, freq0, freq1)
 save_path =save_dir+"\\hyperbolic_chirp.wav"
@@ -67,7 +68,7 @@ signal = sp.data
 fs = sp.sampleRate
 T = sp.fileLength / fs
 TFR_3 = sp.spectrogram(win_len, hop, window_type, sgType=spec_type, sgScale=scale, nfilters=mel_num)
-
+#
 # # plot1: signal
 # figname1 = save_dir + "\\chirp_signal_constant_A.jpg"
 # plt.plot(t, s1)
@@ -75,6 +76,7 @@ TFR_3 = sp.spectrogram(win_len, hop, window_type, sgType=spec_type, sgScale=scal
 # plt.xlabel('Time (seconds)')
 # plt.ylabel('Amplitude')
 # plt.savefig(figname1)
+# plt.close
 
 # # plot2: instantaneous frequency
 # figname2 = save_dir + "\\chirp_signal_instantaneous_frequency.jpg"
@@ -111,29 +113,30 @@ TFR_3 = sp.spectrogram(win_len, hop, window_type, sgType=spec_type, sgScale=scal
 # plt.ylabel('Amplitude')
 # plt.savefig(figname1)
 
-# # plot4: signal
-# figname1 = save_dir + "\\chirp_signal_At.jpg"
-# plt.plot(t, s2)
-# plt.xlim(0,0.05)
-# plt.xlabel('Time (seconds)')
-# plt.ylabel('Amplitude')
-# plt.savefig(figname1)
+# plot4: signal
+figname1 = save_dir + "\\chirp_signal_At.jpg"
+plt.plot(t, s2)
+plt.xlim(0,0.1)
+plt.ylim(-1,1)
+plt.xlabel('Time (seconds)')
+plt.ylabel('Amplitude')
+plt.savefig(figname1)
 
-# plot 3: spectrogram s2
-TFR4 = TFR_3.T
-list_freq_labels = ['0','500', '1000', '1500', '2000', '2500', '3000', '3500', '4000', '4500', '5000', '5500', '6000',
-                   '6500', '7000', '7500', '8000']
-list_time_labels = ['0', '1', '2', '3', '4', '5']
-figname3 = save_dir + '\\chirp_spectrogram_At.jpg'
-# fig, ax = plt.subplots(1, 4, figsize=(10, 20), sharex=True)
-fig, ax = plt.subplots(1, 1)
-im = ax.imshow(np.flipud(TFR4), extent=[0, np.shape(TFR4)[1], 0, np.shape(TFR4)[0]], aspect='auto')
-ax.yaxis.set_major_locator(LinearLocator(17))
-ax.set_yticklabels(list_freq_labels)
-ax.xaxis.set_major_locator(LinearLocator(6))
-ax.set_xticklabels(list_time_labels)
-# ax.set_ylim(0, 8000)
-ax.set_xlabel('Time (seconds)')
-ax.set_ylabel('Frequency (Hz)')
-fig.colorbar(im, ax = ax)
-plt.savefig(figname3)
+# # plot 3: spectrogram s2
+# TFR4 = TFR_3.T
+# list_freq_labels = ['0','500', '1000', '1500', '2000', '2500', '3000', '3500', '4000', '4500', '5000', '5500', '6000',
+#                    '6500', '7000', '7500', '8000']
+# list_time_labels = ['0', '1', '2', '3', '4', '5']
+# figname3 = save_dir + '\\chirp_spectrogram_At.jpg'
+# # fig, ax = plt.subplots(1, 4, figsize=(10, 20), sharex=True)
+# fig, ax = plt.subplots(1, 1)
+# im = ax.imshow(np.flipud(TFR4), extent=[0, np.shape(TFR4)[1], 0, np.shape(TFR4)[0]], aspect='auto')
+# ax.yaxis.set_major_locator(LinearLocator(17))
+# ax.set_yticklabels(list_freq_labels)
+# ax.xaxis.set_major_locator(LinearLocator(6))
+# ax.set_xticklabels(list_time_labels)
+# # ax.set_ylim(0, 8000)
+# ax.set_xlabel('Time (seconds)')
+# ax.set_ylabel('Frequency (Hz)')
+# fig.colorbar(im, ax = ax)
+# plt.savefig(figname3)
