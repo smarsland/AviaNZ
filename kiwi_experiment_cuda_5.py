2029990bfafcecf0fcc7f8e91095d20e05e405d8
VirgyLis <vlistanti.88@gmail.com>
2022-11-10 22:51:46 +1300

    Update kiwi_experiment_cuda_5.py

--- kiwi_experiment_cuda_5.py
+++ kiwi_experiment_cuda_5.py
@@ -475,7 +475,7 @@ norm_list = ["Standard", 'PCEN']
 spectrogram_parameters = {'spec_type':  "Multi-tapered", 'scale': 'Mel Frequency', 'norm_type': None, 'win_len': None, 'hop': None,
                           'window_type': None, 'mel_num': None, 'alpha': None, 'beta': None}
 
-Test_id = 900
+Test_id = 1000
 
 for norm_type in norm_list:
     spectrogram_parameters['norm_type'] = norm_type
@@ -504,10 +504,10 @@ for norm_type in norm_list:
                     spectrogram_parameters['alpha'] = Iatsenko_parameters[0]
                     spectrogram_parameters['beta'] = Iatsenko_parameters[1]
 
-                    if Test_id < 927:
-                        print('\n\n Skipping Test ', Test_id)
-                        Test_id += 1
-                        continue
+                    # if Test_id < 927:
+                    #     print('\n\n Skipping Test ', Test_id)
+                    #     Test_id += 1
+                    #     continue
 
                     print('\n\n Starting Test ', Test_id)
                     print(spectrogram_parameters)
