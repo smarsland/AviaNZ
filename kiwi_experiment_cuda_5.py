eec169a2772aa4fffe6157a9cd5bf99b906598d6
VirgyLis <vlistanti.88@gmail.com>
2022-11-10 22:42:41 +1300

    bug fix

--- kiwi_experiment_cuda_5.py
+++ kiwi_experiment_cuda_5.py
@@ -504,7 +504,7 @@ for norm_type in norm_list:
                     spectrogram_parameters['alpha'] = Iatsenko_parameters[0]
                     spectrogram_parameters['beta'] = Iatsenko_parameters[1]
 
-                    if Test_id < 926:
+                    if Test_id < 927:
                         print('\n\n Skipping Test ', Test_id)
                         Test_id += 1
                         continue
