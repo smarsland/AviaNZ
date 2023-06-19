4720add75740aeccea63e52ee15e44e4b0829e58
smarsland <mathsapteryx@gmail.com>
2023-06-19 22:50:24 +1200

    XML to be tested

--- AviaNZ_batch.py
+++ AviaNZ_batch.py
@@ -279,7 +279,6 @@ class AviaNZ_batchProcess():
             elif self.method == 'Bats':
                 self.exportToBatSearch(self.dirName,threshold1=100,threshold2=None)
                 self.outputBatPasses(self.dirName)
-                #self.exportToBatSearch_NP(self.dirName)
 
             if self.method=='Click' or self.method=='Bats':
                 if not self.CLI:
@@ -1194,6 +1193,100 @@ class AviaNZ_batchProcess():
         # TODO: No error checking!
         # TODO: Use xml properly
         # TODO: Check date
+        from lxml import etree as ET
+
+        # TODO: Get version label!
+        operator = "AviaNZ 3.0"
+        site = "Nowhere"
+        # BatSeach codes
+        namedict = {"Unassigned":0, "Non-bat":1, "Unknown":2, "Long Tail":3, "Short Tail":4, "Possible LT":5, "Possible ST":6, "Both":7}
+        # Set up the XML start
+        start = ET.element("ArrayOfBatRecording")#<?xml version=\"1.0\"?>\n<ArrayOfBatRecording xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
+
+        if not os.path.isdir(dirName):
+            print("Folder doesn't exist")
+            return 0
+        for root, dirs, files in os.walk(dirName, topdown=True):
+            nfiles = len(files)
+            if nfiles > 0:
+                for count in range(nfiles):
+                    filename = files[count]
+                    if filename.endswith('.data'):
+                        s1 = ET.SubElement(start,"BatRecording")
+                        segments = Segment.SegmentList()
+                        segments.parseJSON(os.path.join(root, filename))
+                        if len(segments)>0:
+                            seg = segments[0]
+                            #print(seg)
+                            c = [lab["certainty"] for lab in seg[4]]
+                            s = [lab["species"] for lab in seg[4]]
+                            if len(c)>1:
+                                label = 'Both'
+                            else:
+                                if c[0]>=threshold1:
+                                    if s[0] == 'Long-tailed bat':
+                                        label = 'Long Tail'
+                                    elif s[0] == 'Short-tailed bat':
+                                        label = 'Short Tail'
+                                elif threshold2 is not None:
+                                    if c[0]>threshold2:
+                                        if s[0] == 'Long-tailed bat':
+                                            label = 'Possible LT'
+                                        elif s[0] == 'Short-tailed bat':
+                                            label = 'Possible ST'
+                                elif threshold2 is None:
+                                    if s[0] == 'Long-tailed bat':
+                                        label = 'Possible LT'
+                                    elif s[0] == 'Short-tailed bat':
+                                        label = 'Possible ST'
+                                else:
+                                    label = 'Non-bat'
+                        else:
+                            # TODO: which?
+                            label = 'Non-bat'
+                            #label = 'Unassigned'
+                        # This is the text for the file
+                        s2 = ET.SubElement(s1,"AssignedBatCategory")
+                        s3 = ET.SubElement(s1,"AssignedSite")
+                        s4 = ET.SubElement(s1,"AssignedUser")
+                        s5 = ET.SubElement(s1,"RecTime")
+                        s6 = ET.SubElement(s1,"RecordingFileName")
+                        s7 = ET.SubElement(s1,"RecordingFolderName")
+                        s8 = ET.SubElement(s1,"MeasureTimeFrom")
+
+                        s2.text = str(namedict[label])
+                        s3.text = site
+                        s4.text = operator
+                        # DOC format -- BatSearch wants yyyy-mm-ddThh:mm:ss
+                        if len(filename.split('_')[0]) == 6:
+                            # ddmmyy
+                            timedate = "20"+filename[4:6]+"-"+filename[2:4]+"-"+filename[0:2]+"T"+filename[7:9]+":"+filename[9:11]+":"+filename[11:13]+"</RecTime>\n"
+                        elif len(filename.split('_')[0]) == 8:
+                            # yyyymmdd
+                            timedate = filename[:4]+"-"+filename[4:6]+"-"+filename[6:8]+"T"+filename[9:11]+":"+filename[11:13]+":"+filename[13:15]+"</RecTime>\n"
+                        else:
+                            print("Error: time unknown")
+                            timedate = ""
+                        s5.text = timedate
+
+                        s6.text = filename[:-5]
+                        s7.text = ".\\"+os.path.relpath(root, dirName)
+                        s8.text = str(0)
+
+                # Now write the file if necessary
+                if output != start:
+                    print("writing to", os.path.join(root, savefile))
+                    with open(os.path.join(root, savefile), "wb") as f:
+                        f.write(etree.tostring(etree.ElementTree(start), xml_declaration=True, encoding='utf-8'))
+        return 1
+
+    def exportToBatSearch_2(self,dirName,savefile='BatData.xml',threshold1=0.85,threshold2=0.7):
+        # Write out a file that can be used for BatSearch import
+        # For now, looks like the xml file used there
+        # Assumes that dirName is a survey folder and the structure beneath is something like Rx/Bat/Date
+        # TODO: No error checking!
+        # TODO: Use xml properly
+        # TODO: Check date
         operator = "AviaNZ 3.0"
         site = "Nowhere"
         # BatSeach codes
@@ -1247,10 +1340,17 @@ class AviaNZ_batchProcess():
                         s2 = "<AssignedBatCategory>"+str(namedict[label])+"</AssignedBatCategory>\n"
                         s3 = "<AssignedSite>"+site+"</AssignedSite>\n"
                         s4 = "<AssignedUser>"+operator+"</AssignedUser>\n"
-                        # DOC format
-                        # ddmmyy_hhmmss.bmp not yyyymmdd_hhmmss.bmp
-                        # TODO: Find files! I'm confused!!!
-                        s5 = "<RecTime>"+filename[4:8]+"-"+filename[2:4]+"-"+filename[0:2]+"T"+filename[9:11]+":"+filename[11:13]+":"+filename[13:15]+"</RecTime>\n"
+                        # DOC format -- BatSearch wants yyyy-mm-ddThh:mm:ss
+                        if len(filename.split('_')[0]) == 6:
+                            # ddmmyy
+                            s5 = "<RecTime>"+"20"+filename[4:6]+"-"+filename[2:4]+"-"+filename[0:2]+"T"+filename[7:9]+":"+filename[9:11]+":"+filename[11:13]+"</RecTime>\n"
+                        elif len(filename.split('_')[0]) == 8:
+                            # yyyymmdd
+                            s5 = "<RecTime>"+filename[:4]+"-"+filename[4:6]+"-"+filename[6:8]+"T"+filename[9:11]+":"+filename[11:13]+":"+filename[13:15]+"</RecTime>\n"
+                        else:
+                            print("Error: time unknown")
+                            s5 = "<RecTime>"+"</RecTime>\n"
+
                         #s5 = "<RecTime>"+filename[:4]+"-"+filename[4:6]+"-"+filename[6:8]+"T"+filename[9:11]+":"+filename[11:13]+":"+filename[13:15]+"</RecTime>\n"
                         s6 = "<RecordingFileName>"+filename[:-5]+"</RecordingFileName>\n"
                         s7 = "<RecordingFolderName>.\\"+os.path.relpath(root, dirName)+"</RecordingFolderName>\n"
@@ -1269,7 +1369,7 @@ class AviaNZ_batchProcess():
     
         return 1
 
-    def exportToBatSearch_NP(self, dirName, savefile='BatData.xml'):
+    def exportToBatSearch_1(self, dirName, savefile='BatData.xml'):
         # Write out a file that can be used for BatSearch import
         # For now, looks like the xml file used there
         # Assumes that dirName is a survey folder and the structure beneath is something like Rx/Bat/Date
